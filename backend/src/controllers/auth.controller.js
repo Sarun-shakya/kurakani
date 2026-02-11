65a8f607c9b12f9862cb8a243fac74719c90f663
Sarun-shakya <sarunshakya414@gmail.com>
2026-02-11 21:43:30 +0545

    login and logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,30 +2,31 @@ import User from '../models/user.model.js';
 import bcrypt from 'bcrypt';
 import { generateToken } from '../lib/utils.js';
 
+// Signup 
 export const signup = async (req, res) => {
-    const {fullName, email, password} = req.body;
-    
+    const { fullName, email, password } = req.body;
+
     try {
-        if(!fullName || !email || !password){
+        if (!fullName || !email || !password) {
             return res.status(400).json({
-                message:"All fields are required"
+                message: "All fields are required"
             })
         }
-        if(password.length < 6){
+        if (password.length < 6) {
             return res.status(400).json({
-                message:"Password must be at least 6 characters long"
+                message: "Password must be at least 6 characters long"
             })
         }
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!emailRegex.test(email)){
+        if (!emailRegex.test(email)) {
             return res.status(400).json({
-                message:"Invalid email format"
+                message: "Invalid email format"
             })
         }
 
         // find user
-        const user = await User.findOne({email});
-        if(user){
+        const user = await User.findOne({ email });
+        if (user) {
             return res.status(400).json({
                 message: "Email already exists"
             })
@@ -38,10 +39,10 @@ export const signup = async (req, res) => {
         const newUser = new User({
             fullName,
             email,
-            password:hashedPassword
+            password: hashedPassword
         })
 
-        if(newUser){
+        if (newUser) {
             const savedUser = await newUser.save();
             generateToken(savedUser._id, res);
 
@@ -50,9 +51,9 @@ export const signup = async (req, res) => {
                 fullName: newUser.fullName,
                 email: newUser.email,
                 profilePic: newUser.profilePic,
-                message:"New user created successfully"
+                message: "New user created successfully"
             });
-        }else{
+        } else {
             res.status(400).json({
                 message: "Invalid user data"
             })
@@ -63,4 +64,56 @@ export const signup = async (req, res) => {
             message: "Internal server error"
         });
     }
+}
+
+// Login 
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+
+        // find user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        generateToken(user._id, res);
+
+        res.status(201).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+            message: "User logged in successfully"
+        });
+    } catch (error) {
+        console.error("Error in login controller");
+        res.status(500).json({
+            message:"Internal Server error"
+        })
+    }
+}
+
+// Logout
+export const logout = (_, res) => {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({
+        message:"Logged out successfully"
+    })
 }
